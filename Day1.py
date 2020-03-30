4f7be055256da9d7d487dcbba86023e49b82218f
liukai234 <2679327337@qq.com>
2020-03-30 22:43:52 +0800

    day1

---
+++ Day1.py
@@ -0,0 +1,13 @@
+'''
+@Author: your name
+@Date: 2020-03-30 22:37:08
+@LastEditTime: 2020-03-30 22:39:39
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /pyFile/Day1.py
+'''
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界', sep='+')
+print('hello', 'world', sep='+', end='!') # sep是自动在元素之间补充的分隔字符
+print('goodbye, world', end='!\n')
---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-03-30 22:37:33
+ * @LastEditTime: 2020-03-30 22:38:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /pyFile/README.md
+ -->
+# python学习记录
+# 2020/3/30
