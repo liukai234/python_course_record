dc4381537017081921aebe9c03af40390f912586
liukai234 <2679327337@qq.com>
2020-04-20 12:16:23 +0800

    update 字符串.py

--- 字符串.py
+++ 字符串.py
@@ -2,7 +2,7 @@
 @Description: 字符串
 @LastEditors: liukai
 @Date: 2020-04-19 19:46:09
-@LastEditTime: 2020-04-20 09:45:30
+@LastEditTime: 2020-04-20 12:15:35
 @FilePath: /pyFile/字符串.py
 '''
 
@@ -16,9 +16,9 @@ print("Loading", end = ""),
 
 for i in range(20):
     print(".",end = '', flush = True) # end将替换末尾的'\n'为'', flush = True将print及时写入到终端
-    time.sleep(0.5)
+    time.sleep(0.05)
 
-# 字符串操作
+# 字符串切片
 str = '123456'
 print (str)          # 输出字符串
 print (str[0:-1])    # 输出第一个到倒数第二个的所有字符
@@ -29,13 +29,78 @@ print (str * 2)      # 输出字符串两次，也可以写成 print (2 * str)
 print (str + "TEST") # 连接字符串
 print()
 
-# 数组list
-list = [ '1', 2 , 4.2, '5', 6 ]
-tinylist = [ 'Newtinylist' ]
+str1 = 'hello, world!'
+# 通过内置函数len计算字符串的长度
+print(len(str1)) # 13
+# 获得字符串首字母大写的拷贝
+print(str1.capitalize()) # Hello, world!
+# 获得字符串每个单词首字母大写的拷贝
+print(str1.title()) # Hello, World!
+# 获得字符串变大写后的拷贝
+print(str1.upper()) # HELLO, WORLD!
+# 从字符串中查找子串所在位置
+print(str1.find('or')) # 8
+print(str1.find('shit')) # -1
+# 与find类似但找不到子串时会引发异常
+# print(str1.index('or'))
+# print(str1.index('shit'))
+# 检查字符串是否以指定的字符串开头
+print(str1.startswith('He')) # False
+print(str1.startswith('hel')) # True
+# 检查字符串是否以指定的字符串结尾
+print(str1.endswith('!')) # True
+# 将字符串以指定的宽度居中并在两侧填充指定的字符
+print(str1.center(50, '*'))
+# 将字符串以指定的宽度靠右放置左侧填充指定的字符
+print(str1.rjust(50, ' '))
+str2 = 'abc123456'
+# 检查字符串是否由数字构成
+print(str2.isdigit())  # False
+# 检查字符串是否以字母构成
+print(str2.isalpha())  # False
+# 检查字符串是否以数字和字母构成
+print(str2.isalnum())  # True
+str3 = '  jackfrued@126.com '
+print(str3)
+# 获得字符串修剪左右两侧空格之后的拷贝
+print(str3.strip())
 
-print (list)            # 输出完整列表
-print (list[0])         # 输出列表第一个元素
-print (list[1:3])       # 从第二个开始输出到第三个元素
-print (list[2:])        # 输出从第三个元素开始的所有元素
-print (tinylist * 2)    # 输出两次列表
-print (list + tinylist) # 连接列表
+# 列表list
+list1 = [1, 3, 5, 7, 100]
+# 添加元素
+list1.append(200)
+list1.insert(1, 400)
+# 合并两个列表
+# list1.extend([1000, 2000])
+list1 += [1000, 2000]
+print(list1) # [1, 400, 3, 5, 7, 100, 200, 1000, 2000]
+print(len(list1)) # 9
+# 先通过成员运算判断元素是否在列表中，如果存在就删除该元素
+if 3 in list1:
+	list1.remove(3)
+if 1234 in list1:
+    list1.remove(1234)
+print(list1) # [1, 400, 5, 7, 100, 200, 1000, 2000]
+# 从指定的位置删除元素
+list1.pop(0)
+list1.pop(len(list1) - 1)
+print(list1) # [400, 5, 7, 100, 200, 1000]
+# 清空列表元素
+list1.clear()
+print(list1) # []
+
+# 列表的排序操作
+list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+list2 = sorted(list1)
+# sorted函数返回列表排序后的拷贝不会修改传入的列表
+# 函数的设计就应该像sorted函数一样尽可能不产生副作用
+list3 = sorted(list1, reverse=True)
+# 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+list4 = sorted(list1, key=len)
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+# 给列表对象发出排序消息直接在列表对象上进行排序
+list1.sort(reverse=True)
+print(list1)
--- 模块和函数.py
+++ 模块和函数.py
@@ -2,23 +2,29 @@
 @Description: 模块和函数
 @LastEditors: liukai
 @Date: 2020-04-19 19:45:44
-@LastEditTime: 2020-04-20 09:51:11
+@LastEditTime: 2020-04-20 12:07:53
 @FilePath: /pyFile/模块和函数.py
 '''
- 
-import random # 随机数
+
+from 完数 import call  # 模块导入，如果模块中有主函数，会自动执行模块
+import random  # 随机数
 sum = 0
 answer = random.randint(1, 100)
 
-#九九乘法表
+# 九九乘法表
 for i in range(1, 10):
     for j in range(1, i + 1):
-        print('%d*%d=%d' % (i, j, i * j), end='\t')
+        # print('%d*%d=%d' % (i, j, i * j), end='\t') # 方法一
+        # print('{0} * {1} = {2}'.format(a, b, a * b)) # 方法二
+        """
+        Python 3.6以后，格式化字符串还有更为简洁的书写方式，
+        就是在字符串前加上字母f，我们可以使用下面的语法糖来简化上面的代码
+        """
+        print(f'{i} * {j} = {i * j}', end='\t')
     print()
 
 # 从模块'完数'中导入call函数
-from 完数 import call # 模块导入，如果模块中有主函数，会自动执行模块
 n = 100
 for ite in range(1, n + 1, 1):
-        if call(ite) == True:
-            print(ite)
+    if call(ite) == True:
+        print(ite)
