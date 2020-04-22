0a1a4d5a14cdee5d22daa24ad1ea871f919ff37c
Amore-e <3540433804@qq.com>
2020-04-22 23:52:00 +0800

    1

--- 面向对象/修改器和访问器.py
+++ 面向对象/修改器和访问器.py
@@ -1,8 +1,8 @@
 '''
-@Description: 
+@Description: 修改器和访问器
 @LastEditors: liukai
 @Date: 2020-04-21 18:57:32
-@LastEditTime: 2020-04-21 19:42:20
+@LastEditTime: 2020-04-21 20:50:43
 @FilePath: /pyFile/面向对象/面向对象进阶.py
 '''
 
@@ -14,28 +14,37 @@
 如果要做到这点，就可以考虑使用@property包装器来包装getter和setter方法，使得对属性的访问既安全又方便
 '''
 
+
 class Person(object):
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+    def __init__(self, str):
+        self._str = str
 
     # 访问器 - getter方法
     @property
-    def age(self):
-        return self._age
+    def str(self):
+        return self._str
 
     # 修改器 - setter方法
-    @age.setter
-    def age(self, age):
-        self._age = age
+    @str.setter
+    def str(self, str):
+        self._str = str
+
+    # 不使用访问器和修改器时，在类中写
+    # def get_str(self):
+    # return self._str
+
+    # def set_str(self, str):
+    # self._str = str
 
 
 def main():
-    person = Person('1', 1)
-    print(person.age)
-    print(person._age)
-    person.age=2
-    print(person.age)
+    person = Person('init_string')
+    person.str = 'modify_string'  # 使用修改器
+    # person.set_score('modify_string') # 不使用修改器
+    print(person.str)  # 使用访问器 意义在于不直接访问受保护成员
+    # person.get_score() # 不使用访问器
+    print(person._str)  # 直接访问受保护成员，不建议这种写法
+
 
 if __name__ == "__main__":
     main()
