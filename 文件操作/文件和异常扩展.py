79077aa8dae606b19a66f88411a504149d0cf69e
liukai234 <2679327337@qq.com>
2020-04-24 19:22:55 +0800

    init file And and 创建单个目录

--- 文件操作/文件和异常扩展.py
+++ 文件操作/文件和异常扩展.py
@@ -2,7 +2,7 @@
 @Description: 
 @LastEditors: liukai
 @Date: 2020-04-23 09:09:54
-@LastEditTime: 2020-04-23 10:26:41
+@LastEditTime: 2020-04-24 19:21:17
 @FilePath: /pyFile/文件操作/文件和异常扩展.py
 '''
 
@@ -34,7 +34,7 @@ with os.scandir('scandir') as entries:
 
 # 使用pathlib.Path
 from pathlib import Path
-
+import datetime
 # athlib 提供了一组类，以简单并且面向对象的方式提供了路径上的大多数常见的操作。使用 pathlib
 # 比起使用 os 中的函数更加有效。和 os 相比，使用 pathlib 的另一个好处是减少了操作文件系统路
 # 径所导入包或模块的数量
@@ -45,4 +45,44 @@ entries = Path('scandir')
 for entry in entries.iterdir(): 
     # 由.iterdir()生成的每个条目都包含文件或目录的信息
     # 为文件系统提供了面向对象的接口
-    print(entry.name)
+    if entry.is_file(): # 进行文件过滤
+        print(entry.name)
+
+"""                                                                           
+转换 UNIX 时间戳为 datetime对象                                                       
+:param timestamp: 时间戳                                                         
+:param convert_to_local: 是否转为本地时间                                             
+:param utc: 时区信息，中国为utc+8                                                     
+:param is_remove_ms: 是否去除毫秒                                                   
+:return: datetime 对象                                                          
+"""                                                                           
+def timestamp2datetime(timestamp, convert_to_local=True, utc=8, is_remove_ms=True):
+    if is_remove_ms:
+        timestamp = int(timestamp)                                                
+    dt = datetime.datetime.utcfromtimestamp(timestamp)                            
+    if convert_to_local:                                                          
+        dt = dt + datetime.timedelta(hours=utc)                                   
+    return dt                                                                     
+
+# dosomething 补充时间格式转换
+def convert_date(timestamp, format='%Y-%m-%d %H:%M:%S'):                          
+    dt = timestamp2datetime(timestamp)                                            
+    return dt.strftime(format)             
+
+# 通过生成器来将上述更加简化一些
+print('->生成器和生成式')
+# 检测文件为entry.is_file() 检测目录为entry.is_dir()
+files_in_entries = (entry for entry in entries.iterdir() if entry.is_dir())
+for item in files_in_entries:
+    print('目录名：{} 最后修改时间：{}'.format(item.name, timestamp2datetime(item.stat().st_mtime)))
+
+# 创建单个目录
+p = Path('sub4')
+try:
+    p.mkdir()
+    # p.mkdir(exist_ok=True) 通过exist_ok来忽略异常
+except FileExistsError as e:
+    print(e)
+
+# #
+# 后边留着我接着补
