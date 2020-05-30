cb7b720b9161bf724dc80ee686241cece8620c39
liukai234 <2679327337@qq.com>
2020-05-30 11:03:01 +0800

    complete 静态文件和页面路由

--- python_web/flasky/static.py
+++ python_web/flasky/static.py
@@ -1,1 +1,15 @@
 # 静态文件
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 通过路由处理页面间的跳转
+@app.route('/r404/')
+def r404():
+    return render_template('404.html')
+
+app.run(host='0.0.0.0', port=19722)
