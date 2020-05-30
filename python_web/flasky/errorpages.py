f5278febbd86b034751a918a41f893b6c73e7467
liukai234 <2679327337@qq.com>
2020-05-30 10:07:27 +0800

    init 静态文件

--- python_web/flasky/errorpages.py
+++ python_web/flasky/errorpages.py
@@ -1,1 +1,17 @@
 # 自定义错误页面
+# 404 客户端请求未知页面或路由
+# 500 应用有未处理的异常
+
+from flask import Flask
+from flask import render_template
+app=Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+app.run(host='0.0.0.0', port=19722)
