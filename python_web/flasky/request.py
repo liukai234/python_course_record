0d889fb6dcc789faffbf34a59ec51a76c0fe4986
liukai234 <2679327337@qq.com>
2020-05-30 08:24:39 +0800

    add 响应

--- python_web/flasky/request.py
+++ python_web/flasky/request.py
@@ -26,4 +26,10 @@ app = Flask(__name__)
 # remote_addr
 # environ
 
-# 请求钩子(狗子)
+# 请求钩子(狗子) (通过装饰器实现)
+# before_request
+# before_first_request
+# after_request
+# teardown_request
+
+# 在钩子函数和视图函数之间共享数据使用上下文全局变量g
