c190a05765a86b10a9886dace87a21c9ad8efb07
liukai234 <2679327337@qq.com>
2020-05-30 09:25:00 +0800

    complete templete and init errorpages

--- python_web/flasky/template.py
+++ python_web/flasky/template.py
@@ -1,1 +1,20 @@
 # flask extenion
+# Jinja2 模板引擎
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # 使用的默认路径是 ./templates/index.html
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    # 左边的参数表示模板中使用的占位符，右边表示当前作用域中的变量
+    # 两边使用相同的变量名很常见，但不是强制要求
+    return render_template('user.html', name = name)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=19722, debug=True)
